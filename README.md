4b5a17f99d64349a15a30063bb493d6109214cc4
RedMedian <info@redmedian.ru>
2016-11-04 00:50:55 +0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,21 @@
+## Run
+```
+$ node server.js
+```
+
+## Registers the following routes:
+```
+GET /api/resources
+GET /api/resources/:id
+POST /api/resources
+PUT /api/resources/:id
+DELETE /api/resources/:id
+```
+
+## Testing through the Postman (JSON):
+```
+{
+    "title": "I was born",
+    "year": 1983
+}
+```
---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    morgan = require('morgan'),
+    restful = require('node-restful'),
+    mongoose = restful.mongoose;
+var app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+app.use(methodOverride());
+
+mongoose.connect("mongodb://localhost/resources");
+
+var Resource = app.resource = restful.model('resource', mongoose.Schema({
+    title: String,
+    year: Number,
+  }))
+  .methods(['get', 'post', 'put', 'delete']);
+
+Resource.register(app, '/api/resources');
+
+app.listen(3000);
+console.log('Server is running at port 3000');
