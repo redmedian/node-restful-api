80a2e20833711762b706da98b7b3c26e1a82b1eb
RedMedian <info@redmedian.ru>
2016-11-11 01:29:11 +0300

    Changed mongoose schema

--- server.js
+++ server.js
@@ -22,8 +22,25 @@ app.use(methodOverride());
 mongoose.connect("mongodb://admin:adminpass1010@ds143777.mlab.com:43777/heroku_hgd2t1g7");
 
 var Resource = app.resource = restful.model('resources', mongoose.Schema({
-    title: String,
-    year: Number,
+    orderId: { type: Number, required: true, min: 1 }, // Номер заказа
+    orderStatus: { type: Number, default: 0 }, // Статус заказа
+    orderSum: { type: Number, required: true }, // Сумма заказа
+    orderComment: String, // Комментарий к заказу
+    customerId: Number, // Id заказчика
+    customerName: String, // Имя заказчика
+    customerEmail: { type: String, required: true }, // E-mail заказчика
+    customerPhone: { type: String, required: true }, // Телефон заказчика
+    customerDiscount: Number, // Номер дисконтной карты заказчика
+    deliveryAddress: String, // Адрес доставки
+    deliveryMethod: { type: Number, default: 1 }, // Тип доставки
+    paymentMethod: { type: Number, default: 1 }, // Тип оплаты
+    orderProductList: [{
+        productId : { type: Number, required: true, min: 1 }, // Id товара
+        productQuantity: { type: Number, required: true, min: 1 }, // Количество товара
+        productPrice: { type: Number, required: true }, // Стоимость товара
+        productStore: { type: Number, required: true }, // Номер склада
+    }],
+    createdAt: { type: Date, default: Date.now },
   }))
   .methods(['get', 'post', 'put', 'delete']);
 
